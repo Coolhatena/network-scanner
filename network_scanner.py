9026ef429709a85e672c0134307125570f528f69
Coolhatena <ingsc.geffroy@gmail.com>
2025-05-17 23:58:34 -0700

    Create Device class

--- network_scanner.py
+++ network_scanner.py
@@ -8,4 +8,10 @@ from colorama import init, Fore, Back, Style
 # Inicializar colorama
 init()
 
+class Device:
+    def __init__(self, ip, hostname="Desconocido"):
+        self.ip = ip
+        self.hostname = hostname
+        self.last_seen = datetime.now()
+        self.is_active = True
 
